e5bc49a5f86b920d9266f0b29e04194b40e73ceb
HenryZivers <zivers@live.cn>
2016-05-10 19:01:08 +0800

    update

--- hello.py
+++ hello.py
@@ -2,8 +2,18 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def hellow_world():
+def hello_world():
     return 'Hello World!'
 
+@app.route('/user/<username>')
+def show_user_profile(username):
+    return 'User %s' % username
+
+@app.route('/post/<int:post_id>')
+def show_post(post_id):
+    return 'Post %d' % post_id
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+    #app.run(host='0.0.0.0')
+    app.debug = True
+    app.run()
